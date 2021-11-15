c4cc6f28e6e3017967a2d2246b2b3f3302de9085
Ryo Ota <10933561+nwtgck@users.noreply.github.com>
2021-11-15 00:22:26 +0000

    chore: comment

--- start.bash
+++ start.bash
@@ -14,7 +14,9 @@ trap 'kill $(jobs -p)' EXIT
 # e.g. /nix/store/nki9ywqzbvz68vr75kn2r7g1q84f5agy-python3-3.9.6
 python3_root=$(dirname $(dirname $(which python3)))
 
+# Without chmod permission denied
 chmod +w $python3_root/lib/python3.9/site-packages/
+# Without chmod permission denied
 chmod +w $python3_root/bin/
 pip3 install -r requirements.txt
 
